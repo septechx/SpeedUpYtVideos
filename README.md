9d9b363f6a9a8b5fcf61f9e774ed5b99e34c06ad
septech <septechx@proton.me>
2025-06-05 18:57:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SpeedUpYtVideos
+
+Tampermonkey userscript to speed up youtube videos to a custom speed.
+Compatible with Remove Adblock Thing
---
+++ SpeedUpYtVideos.user.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name         Speed Up Youtube Videos (v2)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Speed up all youtube videos to a custom speed
+// @author       septech
+// @match        https://*.youtube.com/*
+// @match        https://*.youtube-nocookie.com/*
+// @updateURL    https://github.com/septechx/SpeedUpYtVideos/raw/main/SpeedUpYtVideos.user.js
+// @downloadURL  https://github.com/septechx/SpeedUpYtVideos/raw/main/SpeedUpYtVideos.user.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
+
+(() => {
+  "use strict";
+
+  // Configuration
+  const default_speed = 3;
+  const default_enable = true;
+
+  // This class is used to share state between the main script instance and the player script instance when using Remove Adblock Thing
+  class State {
+    constructor(key, defaultState) {
+      this.key = key;
+      this.state ??= defaultState;
+    }
+
+    get state() {
+      return GM_getValue(this.key);
+    }
+
+    set state(newState) {
+      GM_setValue(this.key, newState);
+    }
+  }
+
+  // Generate an id using the URL to identify the script instances
+  const id = window.location.href
+    .split("")
+    .map((char) => char.charCodeAt(0))
+    .reduce((x, y) => x + y)
+    .toString(16);
+
+  const app = document.querySelector("ytd-app");
+  const input = document.createElement("input");
+  input.style =
+    "position: absolute; z-index: 2021; background: #181818; border: #181818; color: #fff;";
+  input.placeholder = "SUYV";
+  document.body.insertBefore(input, app);
+
+  console.log(`[SUYV | ${id}] Created input`);
+
+  const speed = new State("speed", default_speed);
+  const enable = new State("enable", default_enable);
+
+  console.log(`[SUYV | ${id}] Created state`);
+
+  input.value = speed.state;
+
+  input.addEventListener("input", () => {
+    if (!input.value) return;
+
+    try {
+      const requestedSpeed = parseFloat(input.value);
+      speed.state = requestedSpeed;
+      console.log(`[SUYV | ${id}] Set speed to ${speed.state}`);
+
+      speedUpVideos();
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "s") {
+      enable.state = !enable.state;
+      console.log(`[SUYV | ${id}] Set enabled to ${enable.state}`);
+
+      speedUpVideos();
+    }
+  });
+
+  function speedUpVideos() {
+    const videos = document.querySelectorAll("video");
+    videos.forEach((video) => {
+      video.playbackRate = enable.state ? speed.state : 1;
+    });
+  }
+
+  window.addEventListener("load", speedUpVideos);
+  setInterval(speedUpVideos, 1000);
+})();
