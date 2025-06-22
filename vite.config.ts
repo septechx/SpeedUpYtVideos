81b8e011c777b71a80a95fe695cc6e65e333d70f
septech <septechx@proton.me>
2025-06-23 00:37:38 +0200

    Change namespace

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       entry: "src/main.ts",
       userscript: {
         icon: "https://www.google.com/s2/favicons?sz=64&domain=youtube.com",
-        namespace: "seps-scripts",
+        namespace: "siesque",
         match: ["https://*.youtube.com/*", "https://*.youtube-nocookie.com/*"],
         author: "septech",
         name: "Speed Up Yt Videos",
